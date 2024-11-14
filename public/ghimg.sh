ea25903510a57a59dbb9f8b01a2e92359865c787
Scott M Anderson <smanders@sdl.usu.edu>
2024-11-13 18:33:46 -0700

    remove rocky85-[dev|run]
    resolves https://github.com/externpro/buildpro/issues/92

--- public/ghimg.sh
+++ public/ghimg.sh
@@ -5,7 +5,7 @@ if [ -n "$(git status --porcelain --untracked=no)" ] || [[ ${gtag} == *"-g"* ]];
   gtag=latest
 fi
 # build ghcr.io images
-for img in rocky85-pro rocky85-bld rocky85-dev rocky85-run
+for img in rocky85-pro rocky85-bld
 do
   pkg=ghcr.io/externpro/buildpro/${img}:${gtag}
   time docker image build \
