98d47259c4cc1e4272fd8c9d3882117797122319
Scott M Anderson <smanders@sdl.usu.edu>
2024-11-25 12:44:47 -0700

    ghimg: add rocky85-pdv to 'docker image build' list
    issue https://github.com/externpro/buildpro/issues/95

--- public/ghimg.sh
+++ public/ghimg.sh
@@ -5,7 +5,7 @@ if [ -n "$(git status --porcelain --untracked=no)" ] || [[ ${gtag} == *"-g"* ]];
   gtag=latest
 fi
 # build ghcr.io images
-for img in rocky85-pro rocky85-bld rocky85-pin
+for img in rocky85-pro rocky85-bld rocky85-pin rocky85-pdv
 do
   pkg=ghcr.io/externpro/buildpro/${img}:${gtag}
   time docker image build \
